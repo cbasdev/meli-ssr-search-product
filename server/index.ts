965b26bb67eb950f86419b8374a64995deb22f39
Sebastian Velez <sebasttianvelez@gmail.com>
2022-07-14 23:12:16 -0500

    :sparkles: Adding SSR With express

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+/**
+ *
+ * I cant import ESModules but need configuration with babel.
+ *
+ * Pending for refactoring
+ */
+
+const express = require('express')
+const next = require('next')
+
+const port = 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
